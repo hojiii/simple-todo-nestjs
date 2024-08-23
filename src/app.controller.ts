2c1ba6d7fb86499d6906e8e4961146231eeafe15
hojji <shimn3060@naver.com>
2024-08-24 02:03:01 +0900

    fear: nest js 기본 라우팅, 데코레이터

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+//라우팅으로 하고싶은것 입력시 사용 가능("localhost:3000/root")
+@Controller('root')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  //쿼리값을 선언해서 사용 하는 방법 ("localhost:3000/root/이름")
+  @Get(':name')
+  getHello(@Param('name') name: string): string {
+    // return this.appService.getHello();
+    return '라우팅 연습' + name;
   }
 }
